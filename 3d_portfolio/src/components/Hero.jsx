9aa666ea8df50a39b93ccc296f22cdf090e944a5
Rahul Kumar <krrahul4696@gmail.com>
2024-07-30 23:58:25 +0530

    updated work

--- 3d_portfolio/src/components/Hero.jsx
+++ 3d_portfolio/src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Rahul</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop
+            I develop & deploy
              <br className='sm:block hidden' />
             user interfaces and web applications.
           </p>
